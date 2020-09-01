454073fb99e5367566360a0a18fbcd4ef9dfdbbd
amsuvarna27 <54762469+amsuvarna27@users.noreply.github.com>
2020-09-01 18:05:28 +0530

    Add files via upload

---
+++ intellij/Kotlin/Number/src/main/kotlin/main.kt
@@ -0,0 +1,8 @@
+fun main(args: Array<String>) {
+    var num = 1..100
+
+    for(a in num)
+    {
+        println(a)
+    }
+}
---
+++ intellij/Kotlin/list/src/main/kotlin/main.kt
@@ -0,0 +1,8 @@
+fun main(args: Array<String>) {
+    var num = listOf(1,2,3,4,5,6,8)
+    for(i in num)
+    {
+        println(i)
+
+    }
+}
