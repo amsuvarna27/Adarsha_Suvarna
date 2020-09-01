75e2f9e946730a03048af937f7e42ededa3dd5de
amsuvarna27 <54762469+amsuvarna27@users.noreply.github.com>
2020-09-01 18:05:52 +0530

    Add files via upload

---
+++ intellij/Kotlin/StringToInteger/src/main/kotlin/main.kt
@@ -0,0 +1,5 @@
+fun main(args: Array<String>) {
+    var str : String = "9"
+    var num : Int = str.toInt()
+    println(num)
+}
---
+++ intellij/Kotlin/stringComp/src/main/kotlin/main.kt
@@ -0,0 +1,9 @@
+fun main(args: Array<String>) {
+    var str1 : String = "Adarsh"
+    var str2 : String = "Adarsha"
+
+    if(str1.equals(str2))
+        println("Both the strings are equals")
+    else
+        println("Strings are not equals")
+}
