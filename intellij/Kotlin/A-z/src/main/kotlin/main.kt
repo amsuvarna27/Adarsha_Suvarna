3a37f9eff67c97827fd78aabb65ec1299481b3b7
amsuvarna27 <54762469+amsuvarna27@users.noreply.github.com>
2020-09-01 18:00:42 +0530

    Add files via upload

---
+++ intellij/Kotlin/A-z/src/main/kotlin/main.kt
@@ -0,0 +1,10 @@
+fun main(args: Array<String>) {
+    var num = 'A'..'z'
+
+    for(a in num)
+    {
+        println(a)
+    }
+    println("Total count of letters is ${num.count()} ")
+
+}
---
+++ intellij/Kotlin/compare/src/main/kotlin/main.kt
@@ -0,0 +1,11 @@
+fun main(args: Array<String>) {
+    var num1 : Int = 8
+    var num2 : Int = 15
+    var result : Int = 0
+
+    if(num1 > num2)
+        result = num1
+    else
+        result = num2
+    println("Among $num1 and $num2 greater number  is : $result")
+}
